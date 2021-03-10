f72b5f29294a42fc9006d25240da543bc7e6bf32
Geonsang Yoo <geonsangyoo@gmail.com>
2021-03-10 15:50:16 +0900

    initial layout added

--- my_quiz/lib/main.dart
+++ my_quiz/lib/main.dart
@@ -1,2 +1,94 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(QuizApp());
+
+class QuizApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade800,
+        body: SafeArea(child: Quiz()),
+      ),
+    );
+  }
+}
+
+class Quiz extends StatefulWidget {
+  @override
+  _QuizState createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          flex: 7,
+          child: Container(
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.all(50.0),
+                child: Text(
+                  'This is where the question text will go',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+            flex: 3,
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                  height: 80.0,
+                  color: Colors.green,
+                  child: SizedBox.expand(
+                    child: TextButton(
+                      child: Text(
+                        'True',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                      onPressed: () {
+                        print('Button is tapped!');
+                      },
+                    ),
+                  ),
+                ),
+                Container(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                  height: 80.0,
+                  color: Colors.red,
+                  child: SizedBox.expand(
+                    child: TextButton(
+                      child: Text(
+                        'False',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                      onPressed: () {
+                        print('Button is tapped!');
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            )),
+      ],
+    );
+  }
+}
