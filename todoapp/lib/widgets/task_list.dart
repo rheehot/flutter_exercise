536fe52e9d39d0ea7c5b782344e60d417e5104b8
Geonsang Yoo <geonsangyoo@gmail.com>
2021-03-27 22:04:58 +0900

    refactoring

--- todoapp/lib/widgets/task_list.dart
+++ todoapp/lib/widgets/task_list.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:todoapp/widgets/task_tile.dart';
+import 'package:todoapp/models/task.dart';
 
-class TaskList extends StatelessWidget {
+class TaskList extends StatefulWidget {
+  @override
+  _TaskListState createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  List<Task> tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget> [
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return TaskTile(
+            taskTitle: tasks[index].name,
+            isChecked: tasks[index].isDone,
+
+        );
+      },
+      itemCount: tasks.length,
     );
   }
 }
--- todoapp/lib/widgets/task_tile.dart
+++ todoapp/lib/widgets/task_tile.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
 class TaskTile extends StatelessWidget {
+  final bool isChecked;
+  final String taskTitle;
+
+  TaskTile({this.isChecked, this.taskTitle});
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task.'),
+      title: Text(
+        taskTitle,
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
       trailing: Checkbox(
-        value: false,
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
       ),
     );
   }
